e7054c6360ddabf7d77966ba2dfa44dc539f367b
Manas <manaswimulukutla9123@gmail.com>
2026-06-11 15:11:24 +0530

    changed

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -4,14 +4,13 @@ import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
-import AddressImg from "./AddressImg";
+
 import { Fade } from "react-reveal";
 import "./ContactComponent.css";
 import { greeting, contactPageData } from "../../portfolio.js";
 
 const ContactData = contactPageData.contactSection;
-const addressSection = contactPageData.addressSection;
-const phoneSection = contactPageData.phoneSection;
+
 
 class Contact extends Component {
   render() {
@@ -48,44 +47,7 @@ class Contact extends Component {
             </div>
           </Fade>
 
-          <Fade bottom duration={1000} distance="40px">
-            <div className="address-heading-div">
-              <div className="contact-heading-img-div">
-                {/* <img
-											src={require(`../../assets/images/${addressSection["avatar_image_path"]}`)}
-											alt=""
-										/> */}
-                <AddressImg theme={theme} />
-              </div>
-              <div className="address-heading-text-div">
-                <h1
-                  className="address-heading-text"
-                  style={{ color: theme.text }}
-                >
-                  {addressSection["title"]}
-                </h1>
-                <p
-                  className="contact-header-detail-text subTitle"
-                  style={{ color: theme.secondaryText }}
-                >
-                  {addressSection["subtitle"]}
-                </p>
-                <h1
-                  className="address-heading-text"
-                  style={{ color: theme.text }}
-                >
-                  {phoneSection["title"]}
-                </h1>
-                <p
-                  className="contact-header-detail-text subTitle"
-                  style={{ color: theme.secondaryText }}
-                >
-                  {phoneSection["subtitle"]}
-                </p>
 
-              </div>
-            </div>
-          </Fade>
         </div>
         {!this.props.isSection && <Footer theme={this.props.theme} onToggle={this.props.onToggle} />}
         {!this.props.isSection && <TopButton theme={this.props.theme} />}
